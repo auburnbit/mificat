2cf2872e190a95e293b5c38864f2ae4ca8368a21
auburnbit <auburnbit@gmail.com>
2023-02-22 09:45:16 -0600

    Removed separate threads from mificat.py

--- mificat.py
+++ mificat.py
@@ -97,246 +97,167 @@ old_pm4_exp = None
 # Status of our external quiz
 current_quizzing_state = "not currently quizzing"
 
+'''*******************Setting up to READ/WRITE RAM before loop*********************'''
 
+# These will immediately be overwritten
+# These will immediately be overwritten
+ene_mem_read = 255
+exp_mem_read = 0
+gil_mem_read = 0 
+pm1_exp_mem_read = 0
+pm2_exp_mem_read = 0
+pm3_exp_mem_read = 0
+pm4_exp_mem_read = 0
+quiz_ecf_mem_read = 0
 
-def connector_loop_thread():
-    '''Runs in thread t1 and coordinates quiz and RAM editing logic'''
+quiz_flag29_currently_quizzing  = False
+quiz_flag30_correct_answer = True 
 
-    global ene_mem_read
-    global exp_mem_read
-    global gil_mem_read
-    
-    global pm1_exp_mem_read
-    global pm2_exp_mem_read
-    global pm3_exp_mem_read
-    global pm4_exp_mem_read
+current_quizzing_state = "not currently quizzing"
 
-    global quiz_ecf_mem_read
-    global quiz_flag29_currently_quizzing
-    global quiz_flag30_correct_answer
+gm = pymem.Pymem("retroarch.exe")
+qm = pymem.Pymem("Quiz Program.exe")
 
-    global old_pm1_exp
-    global old_pm2_exp
-    global old_pm3_exp
-    global old_pm4_exp
-    
-    global current_quizzing_state
+gameModule = module_from_name(
+    gm.process_handle, "gpsp_libretro.dll").lpBaseOfDll
 
-    # These will immediately be overwritten
-    ene_mem_read = 255
-    exp_mem_read = 0
-    gil_mem_read = 0 
+def getPointerAddress(base, offsets):
+    remote_pointer = RemotePointer(qm.process_handle, base)
+    for offset in offsets:
+        if offset != offsets[-1]:
+            remote_pointer = RemotePointer(qm.process_handle, remote_pointer.value + offset)
+        else:
+            return remote_pointer.value + offset
 
-    pm1_exp_mem_read = 0
-    pm2_exp_mem_read = 0
-    pm3_exp_mem_read = 0
-    pm4_exp_mem_read = 0
+process = filter(lambda p: p.name() == "retroarch.exe",
+                    psutil.process_iter())
+for i in process:
+    ra_pid = i.pid
 
-    quiz_ecf_mem_read = 0
-    
-    quiz_flag29_currently_quizzing  = False
-    quiz_flag30_correct_answer = True 
+psutil.Process(pid=ra_pid).resume()
 
-    current_quizzing_state = "not currently quizzing"
+while True:
 
-    gm = pymem.Pymem("retroarch.exe")
-    qm = pymem.Pymem("Quiz Program.exe")
+    '''*******************coordinates quiz and RAM editing logic*********************'''
 
-    gameModule = module_from_name(
-        gm.process_handle, "gpsp_libretro.dll").lpBaseOfDll
-    
-    def getPointerAddress(base, offsets):
-        remote_pointer = RemotePointer(qm.process_handle, base)
-        for offset in offsets:
-            if offset != offsets[-1]:
-                remote_pointer = RemotePointer(qm.process_handle, remote_pointer.value + offset)
+    if 1 <= ene_mem_read <= 8 :
+
+        #to revert the total exp if later they fail quiz
+        old_pm1_exp = pm1_exp_mem_read
+        old_pm2_exp = pm2_exp_mem_read
+        old_pm3_exp = pm3_exp_mem_read
+        old_pm4_exp = pm4_exp_mem_read
+
+    if current_quizzing_state == "not currently quizzing" and \
+        (ene_mem_read == 9) and \
+        (exp_mem_read > 0 or
+        gil_mem_read > 0):
+        # if the above are true the last enemy just died
+        # but the EXP/gil hasn't been rewarded yet
+        psutil.Process(pid=ra_pid).suspend()
+        ########Hmmmm#########
+        # Maybe I can't set bit 31 because it's the integer sign bit?
+        #####################
+        qm.write_int(getPointerAddress(qm.base_address + 0x03F35120, offsets=[0x2b8, 0x388, 0x108, 0x1a0, 0x70, 0x28, 0x8]), 0X20000000)
+        quiz_flag29_currently_quizzing = 1
+        current_quizzing_state = "started and waiting on user"
+
+    elif current_quizzing_state == "started and waiting on user":
+        # external process is suspended at this point
+        # to prevent game from rewarding EXP/gil yet
+        if quiz_flag29_currently_quizzing == 0:
+            if quiz_flag30_correct_answer == 1:
+                current_quizzing_state = "answer was correct"
             else:
-                return remote_pointer.value + offset
+                current_quizzing_state = "answer was incorrect"        
 
-    process = filter(lambda p: p.name() == "retroarch.exe",
-                     psutil.process_iter())
-    for i in process:
-        ra_pid = i.pid
+    elif current_quizzing_state == "answer was incorrect":
+        psutil.Process(pid=ra_pid).resume()          
+        # punish with no EXP/gil.
+        gm.write_bytes(gameModule + EXP_LOCA,
+                        (0).to_bytes(EXP_SIZE, 'little'), EXP_SIZE)
+        
+        # have to revert the EXP values as they're added too early
+        gm.write_bytes(gameModule + PM1_CEXP_LOCA,
+                    (old_pm1_exp).to_bytes(PM1_CEXP_SIZE, 'little'), PM1_CEXP_SIZE)        
+        gm.write_bytes(gameModule + PM2_CEXP_LOCA,
+                    (old_pm2_exp).to_bytes(PM2_CEXP_SIZE, 'little'), PM2_CEXP_SIZE)       
+        gm.write_bytes(gameModule + PM3_CEXP_LOCA,
+                    (old_pm3_exp).to_bytes(PM3_CEXP_SIZE, 'little'), PM3_CEXP_SIZE)        
+        gm.write_bytes(gameModule + PM4_CEXP_LOCA,
+                    (old_pm4_exp).to_bytes(PM4_CEXP_SIZE, 'little'), PM4_CEXP_SIZE)
 
-    psutil.Process(pid=ra_pid).resume()
+        gm.write_bytes(gameModule + GIL_LOCA,
+                        (0).to_bytes(GIL_SIZE, 'little'), GIL_SIZE)
+        psutil.Process(pid=ra_pid).resume()
+        # and make sure eob timer is done counting
+        if ene_mem_read != 9:
+            current_quizzing_state = "not currently quizzing"
 
-    while True:
+    elif current_quizzing_state == "answer was correct":
+        # leave exp message and gil as they are
+        # and make sure eob timer is done counting
+        psutil.Process(pid=ra_pid).resume()
+        if ene_mem_read != 9:
+            current_quizzing_state = "not currently quizzing"
 
-        if 1 <= ene_mem_read <= 8 :
+    '''****************reading relevant RAM values from external process*******************'''
 
-            #to revert the total exp if later they fail quiz
-            old_pm1_exp = pm1_exp_mem_read
-            old_pm2_exp = pm2_exp_mem_read
-            old_pm3_exp = pm3_exp_mem_read
-            old_pm4_exp = pm4_exp_mem_read
+    prev_ene = ene_mem_read
+    prev_exp = exp_mem_read
+    prev_gil = gil_mem_read
 
-        if current_quizzing_state == "not currently quizzing" and \
-            (ene_mem_read == 9) and \
-            (exp_mem_read > 0 or
-            gil_mem_read > 0):
-            # if the above are true the last enemy just died
-            # but the EXP/gil hasn't been rewarded yet
-            psutil.Process(pid=ra_pid).suspend()
-            ########Hmmmm#########
-            # Maybe I can't set bit 31 because it's the integer sign bit?
-            #####################
-            qm.write_int(getPointerAddress(qm.base_address + 0x03F35120, offsets=[0x2b8, 0x388, 0x108, 0x1a0, 0x70, 0x28, 0x8]), 0X20000000)
-            quiz_flag29_currently_quizzing = 1
-            current_quizzing_state = "started and waiting on user"
+    prev_pm1_exp = pm1_exp_mem_read
+    prev_pm2_exp = pm2_exp_mem_read
+    prev_pm3_exp = pm3_exp_mem_read
+    prev_pm4_exp = pm4_exp_mem_read
 
-        elif current_quizzing_state == "started and waiting on user":
-            # external process is suspended at this point
-            # to prevent game from rewarding EXP/gil yet
-            if quiz_flag29_currently_quizzing == 0:
-                if quiz_flag30_correct_answer == 1:
-                    current_quizzing_state = "answer was correct"
-                else:
-                    current_quizzing_state = "answer was incorrect"        
+    prev_ecf = quiz_ecf_mem_read
 
-        elif current_quizzing_state == "answer was incorrect":
-            psutil.Process(pid=ra_pid).resume()          
-            # punish with no EXP/gil.
-            gm.write_bytes(gameModule + EXP_LOCA,
-                           (0).to_bytes(EXP_SIZE, 'little'), EXP_SIZE)
-            
-            # have to revert the EXP values as they're added too early
-            gm.write_bytes(gameModule + PM1_CEXP_LOCA,
-                        (old_pm1_exp).to_bytes(PM1_CEXP_SIZE, 'little'), PM1_CEXP_SIZE)        
-            gm.write_bytes(gameModule + PM2_CEXP_LOCA,
-                        (old_pm2_exp).to_bytes(PM2_CEXP_SIZE, 'little'), PM2_CEXP_SIZE)       
-            gm.write_bytes(gameModule + PM3_CEXP_LOCA,
-                        (old_pm3_exp).to_bytes(PM3_CEXP_SIZE, 'little'), PM3_CEXP_SIZE)        
-            gm.write_bytes(gameModule + PM4_CEXP_LOCA,
-                        (old_pm4_exp).to_bytes(PM4_CEXP_SIZE, 'little'), PM4_CEXP_SIZE)
-
-            gm.write_bytes(gameModule + GIL_LOCA,
-                           (0).to_bytes(GIL_SIZE, 'little'), GIL_SIZE)
-            psutil.Process(pid=ra_pid).resume()
-            # and make sure eob timer is done counting
-            if ene_mem_read != 9:
-                current_quizzing_state = "not currently quizzing"
-
-        elif current_quizzing_state == "answer was correct":
-            # leave exp message and gil as they are
-            # and make sure eob timer is done counting
-            psutil.Process(pid=ra_pid).resume()
-            if ene_mem_read != 9:
-                current_quizzing_state = "not currently quizzing"
-
-def read_ram_loop_thread():
-    '''
-    Runs in thread t2 and reads relevant RAM values from external process
-    '''
-    global ene_mem_read
-    global exp_mem_read
-    global gil_mem_read
-
-    global pm1_exp_mem_read
-    global pm2_exp_mem_read
-    global pm3_exp_mem_read
-    global pm4_exp_mem_read
-
-    global quiz_ecf_mem_read
-    global current_quizzing_state
-    global quiz_flag29_currently_quizzing
-    global quiz_flag30_correct_answer
-
-    global old_pm1_exp
-    global old_pm2_exp
-    global old_pm3_exp
-    global old_pm4_exp
-
-    # These will immediately be overwritten
-    ene_mem_read = 255
-    exp_mem_read = 0
-    gil_mem_read = 0 
-    pm1_exp_mem_read = 0
-    pm2_exp_mem_read = 0
-    pm3_exp_mem_read = 0
-    pm4_exp_mem_read = 0
-    quiz_ecf_mem_read = 0
-
-    quiz_flag29_currently_quizzing  = False
-    quiz_flag30_correct_answer = True 
-
-    gm = pymem.Pymem("retroarch.exe")
-    qm = pymem.Pymem("Quiz Program.exe")
-
-    gameModule = module_from_name(
-        gm.process_handle, "gpsp_libretro.dll").lpBaseOfDll
+    # Read RAM values (converting to int from byte values)
+    ene_mem_read = int.from_bytes(gm.read_bytes(
+        gameModule + ENE_LOCA, ENE_SIZE), "little")
+    exp_mem_read = int.from_bytes(gm.read_bytes(
+        gameModule + EXP_LOCA, EXP_SIZE), "little")
+    gil_mem_read = int.from_bytes(gm.read_bytes(
+        gameModule + GIL_LOCA, GIL_SIZE), "little")
     
-    def getPointerAddress(base, offsets):
-        remote_pointer = RemotePointer(qm.process_handle, base)
-        for offset in offsets:
-            if offset != offsets[-1]:
-                remote_pointer = RemotePointer(qm.process_handle, remote_pointer.value + offset)
-            else:
-                return remote_pointer.value + offset
+    # reading individual total exp values from game
+    pm1_exp_mem_read = int.from_bytes(gm.read_bytes(
+        gameModule + PM1_CEXP_LOCA, PM1_CEXP_SIZE), "little")
+    pm2_exp_mem_read = int.from_bytes(gm.read_bytes(
+        gameModule + PM2_CEXP_LOCA, PM2_CEXP_SIZE), "little")
+    pm3_exp_mem_read = int.from_bytes(gm.read_bytes(
+        gameModule + PM3_CEXP_LOCA, PM3_CEXP_SIZE), "little")
+    pm4_exp_mem_read = int.from_bytes(gm.read_bytes(
+        gameModule + PM4_CEXP_LOCA, PM4_CEXP_SIZE), "little")  
+    
+    #qm.write_int(getPointerAddress(qm.base_address + 0x03F35120, offsets=[0x2b8, 0x388, 0x108, 0x1a0, 0x70, 0x28, 0x8]), 0x8000000)
+    quiz_ecf_mem_read = int.from_bytes(qm.read_bytes(getPointerAddress(qm.base_address \
+                        + 0x03F35120, offsets=[0x2b8, 0x388, 0x108, 0x1a0, 0x70, 0x28, 0x8]),QUIZ_ECF_SIZE), "little")
+    
+    quiz_flag29_currently_quizzing = (quiz_ecf_mem_read & 0b00100000000000000000000000000000) >> 29
+    quiz_flag30_correct_answer = (quiz_ecf_mem_read & 0b01000000000000000000000000000000) >> 30
+    
+    if current_quizzing_state == "not currently quizzing" and (prev_ene != ene_mem_read or
+                                                    prev_exp != exp_mem_read or
+                                                    prev_gil != gil_mem_read or
+                                                    prev_ecf != quiz_ecf_mem_read or
+                                                    prev_pm1_exp != pm1_exp_mem_read or
+                                                    prev_pm2_exp != pm2_exp_mem_read or
+                                                    prev_pm3_exp != pm3_exp_mem_read or
+                                                    prev_pm4_exp != pm4_exp_mem_read):
 
-    while True:
-        prev_ene = ene_mem_read
-        prev_exp = exp_mem_read
-        prev_gil = gil_mem_read
-
-        prev_pm1_exp = pm1_exp_mem_read
-        prev_pm2_exp = pm2_exp_mem_read
-        prev_pm3_exp = pm3_exp_mem_read
-        prev_pm4_exp = pm4_exp_mem_read
-
-        prev_ecf = quiz_ecf_mem_read
-
-        # Read RAM values (converting to int from byte values)
-        ene_mem_read = int.from_bytes(gm.read_bytes(
-            gameModule + ENE_LOCA, ENE_SIZE), "little")
-        exp_mem_read = int.from_bytes(gm.read_bytes(
-            gameModule + EXP_LOCA, EXP_SIZE), "little")
-        gil_mem_read = int.from_bytes(gm.read_bytes(
-            gameModule + GIL_LOCA, GIL_SIZE), "little")
-        
-        # reading individual total exp values from game
-        pm1_exp_mem_read = int.from_bytes(gm.read_bytes(
-            gameModule + PM1_CEXP_LOCA, PM1_CEXP_SIZE), "little")
-        pm2_exp_mem_read = int.from_bytes(gm.read_bytes(
-            gameModule + PM2_CEXP_LOCA, PM2_CEXP_SIZE), "little")
-        pm3_exp_mem_read = int.from_bytes(gm.read_bytes(
-            gameModule + PM3_CEXP_LOCA, PM3_CEXP_SIZE), "little")
-        pm4_exp_mem_read = int.from_bytes(gm.read_bytes(
-            gameModule + PM4_CEXP_LOCA, PM4_CEXP_SIZE), "little")  
-        
-        #qm.write_int(getPointerAddress(qm.base_address + 0x03F35120, offsets=[0x2b8, 0x388, 0x108, 0x1a0, 0x70, 0x28, 0x8]), 0x8000000)
-        quiz_ecf_mem_read = int.from_bytes(qm.read_bytes(getPointerAddress(qm.base_address \
-                           + 0x03F35120, offsets=[0x2b8, 0x388, 0x108, 0x1a0, 0x70, 0x28, 0x8]),QUIZ_ECF_SIZE), "little")
-        
-        quiz_flag29_currently_quizzing = (quiz_ecf_mem_read & 0b00100000000000000000000000000000) >> 29
-        quiz_flag30_correct_answer = (quiz_ecf_mem_read & 0b01000000000000000000000000000000) >> 30
-        
-        if current_quizzing_state == "not currently quizzing" and (prev_ene != ene_mem_read or
-                                                        prev_exp != exp_mem_read or
-                                                        prev_gil != gil_mem_read or
-                                                        prev_ecf != quiz_ecf_mem_read or
-                                                        prev_pm1_exp != pm1_exp_mem_read or
-                                                        prev_pm2_exp != pm2_exp_mem_read or
-                                                        prev_pm3_exp != pm3_exp_mem_read or
-                                                        prev_pm4_exp != pm4_exp_mem_read):
-
-            print("Enemies: " + f"{ene_mem_read:03d}" + ", " +
-                    "EXP: " + f"{exp_mem_read:05d}" + ", " +
-                    "Gil: " + f"{gil_mem_read:05d}" + ", " +
-                    "ECF: " + f"{quiz_ecf_mem_read:08x}" + ", " +
-                    "Flag29: " + f"{quiz_flag29_currently_quizzing:1d}" + ", " +
-                    "Flag30: " + f"{quiz_flag30_correct_answer:1d}" + ", " +
-                    "PM1 EXP: " + f"{pm1_exp_mem_read:05d}" + ", " +
-                    "PM2 EXP: " + f"{pm2_exp_mem_read:05d}" + ", " +
-                    "PM3 EXP: " + f"{pm3_exp_mem_read:05d}" + ", " +
-                    "PM4 EXP: " + f"{pm4_exp_mem_read:05d}")
-
-
-
-t1 = threading.Thread(target=connector_loop_thread)
-t2 = threading.Thread(target=read_ram_loop_thread)
-
-t1.start()
-t2.start()
+        print("Enemies: " + f"{ene_mem_read:03d}" + ", " +
+                "EXP: " + f"{exp_mem_read:05d}" + ", " +
+                "Gil: " + f"{gil_mem_read:05d}" + ", " +
+                "ECF: " + f"{quiz_ecf_mem_read:08x}" + ", " +
+                "Flag29: " + f"{quiz_flag29_currently_quizzing:1d}" + ", " +
+                "Flag30: " + f"{quiz_flag30_correct_answer:1d}" + ", " +
+                "PM1 EXP: " + f"{pm1_exp_mem_read:05d}" + ", " +
+                "PM2 EXP: " + f"{pm2_exp_mem_read:05d}" + ", " +
+                "PM3 EXP: " + f"{pm3_exp_mem_read:05d}" + ", " +
+                "PM4 EXP: " + f"{pm4_exp_mem_read:05d}")
 
 #NEXT refactor all this mess into something more readable
 #NEXT2 I feel like this should be possible without the two separate threads
